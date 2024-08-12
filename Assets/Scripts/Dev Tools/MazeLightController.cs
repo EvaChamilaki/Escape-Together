0465d96eece416a2377ef187835a5aa0b7941071
pclank <31415965+pclank@users.noreply.github.com>
2024-08-12 16:30:35 +0300

    added point lights to the scene, along with a tool to switch between directional and the latter.

---
+++ Assets/Scripts/Dev Tools/MazeLightController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Dev_Tools
+{
+    public class MazeLightController : ScriptableObject
+    {
+        [MenuItem("Tools/Light Controller/Switch to Point Lights")]
+        static void SwitchToPoints()
+        {
+            foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+            {
+                if (go.hideFlags != HideFlags.None)
+                    continue;
+
+                if (PrefabUtility.GetPrefabType(go) == PrefabType.Prefab || PrefabUtility.GetPrefabType(go) == PrefabType.ModelPrefab)
+                    continue;
+
+                if (go.CompareTag("DirectionalLight"))
+                    go.SetActive(false);
+                else if (go.CompareTag("MazeLight"))
+                    go.SetActive(true);
+            }
+
+            EditorUtility.DisplayDialog("Light Controller", "Switched to point lights!", "OK", "");
+        }
+        [MenuItem("Tools/Light Controller/Switch to Directional Light")]
+        static void SwitchToDirectional()
+        {
+            foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+            {
+                if (go.hideFlags != HideFlags.None)
+                    continue;
+
+                if (PrefabUtility.GetPrefabType(go) == PrefabType.Prefab || PrefabUtility.GetPrefabType(go) == PrefabType.ModelPrefab)
+                    continue;
+
+                if (go.CompareTag("DirectionalLight"))
+                    go.SetActive(true);
+                else if (go.CompareTag("MazeLight"))
+                    go.SetActive(false);
+            }
+
+            EditorUtility.DisplayDialog("Light Controller", "Switched to directional!", "OK", "");
+        }
+    }
+}
