880dc342ee40adb3bf6cdcc17b22f338d36a2911
pclank <31415965+pclank@users.noreply.github.com>
2024-08-21 16:14:28 +0300

    added dynamic volume adjustment to deco piano.

--- Assets/Scripts/DecoPianoPlayer.cs
+++ Assets/Scripts/DecoPianoPlayer.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class DecoPianoPlayer : MonoBehaviour
 {
+    public AudioSource main_music_src;
+
     private uint music_index = 0;
     private uint prev_music_index = 0;
 
     private float timer_start, timer_val;
     private float delay = 2.0f;
 
+    private bool has_started = false;
+
     // Start is called before the first frame update
     void Start()
     {
         music_index = 0;
+        has_started = false;
     }
 
     public bool IsPlaying()
@@ -24,8 +29,20 @@ public class DecoPianoPlayer : MonoBehaviour
         if (GetComponents<AudioSource>()[prev_music_index].isPlaying)
             return;
 
+        main_music_src.volume = 0.05f;
+        has_started = true;
+
         GetComponents<AudioSource>()[music_index].Play();
         prev_music_index = music_index;
         music_index = (music_index == 4) ? 0 : music_index + 1;
+    }
+
+    void Update()
+    {
+        if (has_started && !GetComponents<AudioSource>()[prev_music_index].isPlaying)
+        {
+            main_music_src.volume = 0.1f;
+            has_started = false;
+        }
     }
 }
